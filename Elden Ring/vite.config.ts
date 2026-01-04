060b740f00e34618dbca0e0d3a5913813c632422
Papposourusus <masikpatrik@gmail.com>
2026-01-04 22:13:56 +0100

    Update vite.config.ts

--- Elden Ring/vite.config.ts
+++ Elden Ring/vite.config.ts
@@ -1,13 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/elden-ring-vue/', // názov tvojho repo
+  base: '/ft_zaverecny_projek/',
 
   plugins: [
     vue(),
