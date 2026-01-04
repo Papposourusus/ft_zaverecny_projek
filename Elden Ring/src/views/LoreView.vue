e2470ad9645179dd56368b01ae043d166a9d8843
Papposourusus <masikpatrik@gmail.com>
2026-01-04 19:10:12 +0100

    View

---
+++ Elden Ring/src/views/LoreView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Lore</h1>
+  </div>
+</template>
+
+<script>
+export default { name: "LoreView" }
+</script>
---
+++ Elden Ring/src/views/ZbraneView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Bossovia</h1>
+  </div>
+</template>
+
+<script>
+export default { name: "BossoviaView" }
+</script>
