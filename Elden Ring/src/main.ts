0af37cc4fa4682ccc1f11e37774a81917b9f4eb3
Papposourusus <masikpatrik@gmail.com>
2026-01-04 19:38:47 +0100

    img

--- Elden Ring/src/main.ts
+++ Elden Ring/src/main.ts
@@ -4,6 +4,12 @@ import router from './router'
 
 // Globálne CSS
 import './assets/style.css'
+import './assets/main.css'
+import './assets/base.css'
+import './assets/bossovia.css'
+import './assets/zbrane.css'
+import './assets/lore.css'
+import './assets/kontakt.css'
 
 createApp(App)
   .use(router)
