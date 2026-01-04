637f5b6dcbed67d99eb70679665e45573c1ac131
Papposourusus <masikpatrik@gmail.com>
2026-01-04 22:56:46 +0100

    Revert "y"
    This reverts commit 60f80371e3994d7072f3eb95ca89904f517e7b80.

--- Elden Ring/vite.config.ts
+++ Elden Ring/vite.config.ts
@@ -1,9 +1,3 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import { fileURLToPath, URL } from 'node:url'
-
 export default defineConfig({
   base: '/ft_zaverecny_projek/',
 
